a560a0e496ce3dd7a3f09176893ee467c31a29a0
prerak13 <choksiprerak@gmail.com>
2019-12-12 13:39:45 +0530

    jdbc auth

--- src/main/java/com/example/demo/security/SecurityConfiger.java
+++ src/main/java/com/example/demo/security/SecurityConfiger.java
@@ -1,18 +1,24 @@
 package com.example.demo.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiger extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource ds;
 @Override
 protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	auth.inMemoryAuthentication().withUser("USER").password("USER").roles("USER")
-	.and().withUser("ADMIN").password("ADMIN").roles("ADMIN").and();
+	auth.jdbcAuthentication()
+	.dataSource(ds);
 }
 
 @Bean
